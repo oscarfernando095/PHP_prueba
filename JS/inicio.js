2018e7e1cd32adc9d1aba6e8c764ebafcd259963
oscar <oscarfernando271968@gmail.com>
2024-06-01 14:38:54 -0300

    cambio

--- JS/inicio.js
+++ JS/inicio.js
@@ -1,16 +1,21 @@
-console.log("Comienzo javascript");
 
-let inputUsuario = document.getElementById("correo");
-let inputContrasena = document.getElementById("pass");
+let inputUsuario = document.getElementById("correo")
+let inputContrasena = document.getElementById("pass")
+
+let usuario = inputUsuario.value
+let sena = inputContrasena.value
+
+if(usuario ==="" && sena ===""){
+    console.log("Ingresa los datos")
+    alert("Datos Erroneos")
+    }else{
+    console.log("Estas Registrado")
+    alert("Estas Registrado")
+    }
 
 
-let usuario = inputUsuario.value;
-let sena = inputContrasena.value;
 
-console.log(usuario);
-
-/*console.log(document.getElementById("pass").value)
-console.log(document.getElementById("correo").value)*/
-console.log("Hola , no me sale");
+console.log(document.getElementById("correo").value)
+console.log(document.getElementById("pass").value)
 
 
