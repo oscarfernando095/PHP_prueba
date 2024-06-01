b52421ac41093adf833f11398b5f727b9f669f84
oscar <oscarfernando271968@gmail.com>
2024-06-01 11:54:03 -0300

    subo archivos

---
+++ JS/inicio.js
@@ -0,0 +1,16 @@
+console.log("Comienzo javascript");
+
+let inputUsuario = document.getElementById("correo");
+let inputContrasena = document.getElementById("pass");
+
+
+let usuario = inputUsuario.value;
+let sena = inputContrasena.value;
+
+console.log(usuario);
+
+/*console.log(document.getElementById("pass").value)
+console.log(document.getElementById("correo").value)*/
+console.log("Hola , no me sale");
+
+
---
+++ JS/registro.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', (event) =>{
+
+})
+
+
+console.log(document.getElementById("nombre").value)
